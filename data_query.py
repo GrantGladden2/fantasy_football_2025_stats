6104576a31f3a67a5da38b50b6f53c35509950d5
Grant Gladden <ggladden02@gmail.com>
2026-08-05 19:57:46 -0400

    Commit - Cleaned data and started creating the function to split the data

---
+++ data_query.py
@@ -0,0 +1,41 @@
+# Import necessary packages
+import pandas as pd
+import sqlite3
+
+# This function will read in the file and drop the necessary columns
+def read_csv(csv_file):
+
+    # Read the csv file to a dataframe
+    df = pd.read_csv(csv_file, header=1)
+
+    # Drop unnecessary columns
+    df.drop(columns=['Rk','Games Started','FantPt','DKPt','FDPt','VBD','PosRank','OvRank'],inplace=True)
+
+    # Clean the other data that needs to be cleaned
+    for i in range(len(df)):
+        current_player = df.loc[i, 'Player']
+        player = current_player.rstrip("*+")
+        df.replace(current_player,player,inplace=True)
+
+    return df
+
+# This function will split the data into separate tables to be later queried
+def split_data(start_col,end_col,df):
+
+    # If the name of the column does not exist, report back
+    if start_col not in df.columns:
+        print("That column name does not exist does not exist")
+
+    # If end is blank, assume there's only one column
+
+
+    # Create a new data frame using the table and start and end cols provided
+
+
+    pass
+
+# This function will query the different tables
+def query_data():
+
+    # Any players with all 0s, remove from the table
+    pass
--- main.py
+++ main.py
@@ -1,6 +1,31 @@
 # Main function for fantasy football 2025 season stats
 # Import packages as needed
-from create_tables import *
+from data_query import *
 
+# Input csv file name and use the function to create the pandas dataframe
 csv_file = 'fantasy_football_stats_2025.csv'
-df = split_tables(csv_file)
+df = read_csv(csv_file)
+
+# Input the start and end columns for each section to split
+# The function will make a new dataframe with just those columns
+
+# Create a new table for the games section
+games_table = split_data('Player','Games',df)
+
+# Create a new table for the passing section
+passing_table = split_data('Complete','Int',df)
+
+# Create a new table for the rushing section
+rushing_table = split_data('Att','TD',df)
+
+# Create a new table for the receiving section
+receiving_table = split_data('Tgt','TD',df)
+
+# Create a new table for the fumbles section
+fumbles_table = split_data('Fmb','FL',df)
+
+# Create a new table for the scoring section
+scoring_table = split_data('TD','2PP',df)
+
+# Create a new table for the fantasy section
+fantasy_table = split_data('PPR','',df)
