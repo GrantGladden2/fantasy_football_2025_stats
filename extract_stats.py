b8840297c978312930c11b60f06e004ad44bbfa2
Grant Gladden <ggladden02@gmail.com>
2026-08-05 15:20:54 -0400

    Initial Commit - Table created from HTML parsing

---
+++ extract_stats.py
@@ -0,0 +1,47 @@
+# This function should take in the ESPN website of data
+# and transform it into a html file
+import requests
+from bs4 import BeautifulSoup
+
+def web_to_table(website_link):
+
+    # Read in the website link to a variable
+    response = requests.get(website_link)
+
+    # Create the text file from the requested link
+    html = BeautifulSoup(response.content, 'html.parser')
+
+    # Find the table of stats
+    table = html.find('table', class_="stats")
+
+    # Parse through the table to find each header
+    header_row = table.find('thead').find_all('tr', class_='header')[1]
+
+    # Initialize the header list
+    headers = []
+
+    # Loop through the html language to find the headers of the table
+    for th in header_row.find_all('th'):
+        headers.append(th.get("data-id"))
+
+    # Parse through the html body of the table
+    stats_row = table.find('tbody').find_all('td')
+
+    # Initialize the lists
+    stats = []
+    current_player = []
+    i = 0
+
+    # Loop through each item in the stats list
+    for td in stats_row:
+        current_player.append(td.get_text())
+        i += 1
+
+        # If the list gets to the same length as the header list, create a new list
+        if i == len(headers):
+            stats.append(current_player)
+            current_player = []
+            i = 0
+            continue
+
+    return headers, stats
---
+++ main.py
@@ -0,0 +1,14 @@
+# Fantasy Football 2025 Season Stats
+from extract_stats import *
+import pandas as pd
+
+# Objective of this file:
+# 1. Load in data from the online ESPN website into an html file
+web_link = 'https://fantasydata.com/nfl/fantasy-football-leaders?page=1'
+headers, stats = web_to_table(web_link)
+
+# 2. Create a dataframe based on the headers and data collected
+fantasy_football_df = pd.DataFrame(stats,columns=headers)
+
+# 3. Export a CSV file from the giant table
+fantasy_football_df.to_csv('fantasy_football_stats.csv')
