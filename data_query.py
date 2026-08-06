6917bc411500219322137768081677968a1b2a4a
Grant Gladden <ggladden02@gmail.com>
2026-08-05 20:10:12 -0400

    Commit - Created the different tables of data

--- data_query.py
+++ data_query.py
@@ -21,18 +21,20 @@ def read_csv(csv_file):
 
 # This function will split the data into separate tables to be later queried
 def split_data(start_col,end_col,df):
-
     # If the name of the column does not exist, report back
     if start_col not in df.columns:
         print("That column name does not exist does not exist")
 
     # If end is blank, assume there's only one column
-
+    if  end_col == '':
+        table = df[start_col]
+        return table
 
     # Create a new data frame using the table and start and end cols provided
+    table = df.loc[:, start_col:end_col]
 
 
-    pass
+    return table
 
 # This function will query the different tables
 def query_data():
--- main.py
+++ main.py
@@ -9,23 +9,39 @@ df = read_csv(csv_file)
 # Input the start and end columns for each section to split
 # The function will make a new dataframe with just those columns
 
+
 # Create a new table for the games section
 games_table = split_data('Player','Games',df)
+
+
+# games_table.to_csv('games.csv', index=False)
 
 # Create a new table for the passing section
 passing_table = split_data('Complete','Int',df)
 
+# passing_table.to_csv('passing.csv', index=False)
+
 # Create a new table for the rushing section
-rushing_table = split_data('Att','TD',df)
+rushing_table = split_data('Att','TD.2',df)
+
+# rushing_table.to_csv('rushing.csv', index=False)
 
 # Create a new table for the receiving section
-receiving_table = split_data('Tgt','TD',df)
+receiving_table = split_data('Tgt','TD.3',df)
+
+# receiving_table.to_csv('receiving.csv', index=False)
 
 # Create a new table for the fumbles section
 fumbles_table = split_data('Fmb','FL',df)
 
+# fumbles_table.to_csv('fumbles.csv', index=False)
+
 # Create a new table for the scoring section
-scoring_table = split_data('TD','2PP',df)
+scoring_table = split_data('2PM','2PP',df)
+
+# scoring_table.to_csv('scoring.csv', index=False)
 
 # Create a new table for the fantasy section
 fantasy_table = split_data('PPR','',df)
+
+# fantasy_table.to_csv('fantasy.csv', index=False)
