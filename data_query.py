04bb1e85bc620c19c01d7f7d73060a5c7f9ae489
Grant Gladden <ggladden02@gmail.com>
2026-08-05 21:51:32 -0400

    Commit - All csv files loaded

--- data_query.py
+++ data_query.py
@@ -9,7 +9,7 @@ def read_csv(csv_file):
     df = pd.read_csv(csv_file, header=1)
 
     # Drop unnecessary columns
-    df.drop(columns=['Rk','Games Started','FantPt','DKPt','FDPt','VBD','PosRank','OvRank'],inplace=True)
+    df.drop(columns=['Rk','GS','TD_tot','FantPt','DKPt','FDPt','VBD','PosRank','OvRank'],inplace=True)
 
     # Clean the other data that needs to be cleaned
     for i in range(len(df)):
@@ -31,27 +31,32 @@ def split_data(start_col,end_col,df):
     # If end is blank, assume there's only one column
     if  end_col == '':
         table = df[start_col]
-        table = pd.concat([df['Player'], table, df['player_id']], axis=1)
+        table = pd.concat([table, df['player_id']], axis=1)
 
     else:
         # Create a new data frame using the table and start and end cols provided
         table = df.loc[:, start_col:end_col]
 
-        if 'Player' not in table.columns:
-            table = pd.concat([df['Player'],table], axis=1)
-
         if 'player_id' not in table.columns:
             table = pd.concat([table,df['player_id']], axis=1)
 
     # Clean the new table and return
-    # table = query_data(table)
+    table = query_data(table)
     return table
 
 # This function will query the different tables
 def query_data(table):
 
+    drop_indices = []
+
     # Any players with all 0s, remove from the table
-    # for row in table.itertuples():
-    #     print(row)
+    for index, row in table.iterrows():
+        query_row = row[:-1]
+
+        bool_drop = all(stat == 0 for stat in query_row)
+        if bool_drop:
+            drop_indices.append(index)
+
+    table = table.drop(drop_indices)
 
     return table
--- main.py
+++ main.py
@@ -3,44 +3,44 @@
 from data_query import *
 
 # Input csv file name and use the function to create the pandas dataframe
-csv_file = 'fantasy_football_stats_2025.csv'
+csv_file = 'fantasy_football_stats_2025_OG.csv'
 df = read_csv(csv_file)
-
+df.to_csv('fantasy_football_stats_2025.csv', index=False)
 # Input the start and end columns for each section to split
 # The function will make a new dataframe with just those columns
 
 
 # Create a new table for the games section
-games_table = split_data('Player','Games',df)
+games_table = split_data('Player','G',df)
 # Export table to csv
-# games_table.to_csv('games.csv', index=False)
+games_table.to_csv('games.csv', index=False)
 
 # Create a new table for the passing section
-passing_table = split_data('Complete','Int',df)
+passing_table = split_data('Cmp_pass','Int',df)
 # Export table to csv
-# passing_table.to_csv('passing.csv', index=False)
+passing_table.to_csv('passing.csv', index=False)
 
 # Create a new table for the rushing section
-rushing_table = split_data('Att','TD.1',df)
+rushing_table = split_data('Att_rush','TD_rush',df)
 # Export table to csv
-# rushing_table.to_csv('rushing.csv', index=False)
+rushing_table.to_csv('rushing.csv', index=False)
 
 # Create a new table for the receiving section
-receiving_table = split_data('Tgt','TD.3',df)
+receiving_table = split_data('Tgt_rec','TD_rec',df)
 # Export table to csv
-# receiving_table.to_csv('receiving.csv', index=False)
+receiving_table.to_csv('receiving.csv', index=False)
 
 # Create a new table for the fumbles section
 fumbles_table = split_data('Fmb','FL',df)
 # Export table to csv
-# fumbles_table.to_csv('fumbles.csv', index=False)
+fumbles_table.to_csv('fumbles.csv', index=False)
 
 # Create a new table for the scoring section
 scoring_table = split_data('2PM','2PP',df)
 # Export table to csv
-# scoring_table.to_csv('scoring.csv', index=False)
+scoring_table.to_csv('scoring.csv', index=False)
 
 # Create a new table for the fantasy section
 fantasy_table = split_data('PPR','',df)
 # Export table to csv
-# fantasy_table.to_csv('fantasy.csv', index=False)
+fantasy_table.to_csv('fantasy.csv', index=False)
