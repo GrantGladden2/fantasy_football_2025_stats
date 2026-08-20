ddc7c3caefca77c007ecfcd2542859db260b900a
Grant Gladden <ggladden02@gmail.com>
2026-08-20 16:08:02 -0400

    FIle Cleanup

--- src/python/link_to_table.py
+++ src/python/link_to_table.py
@@ -1,5 +1,5 @@
 # Fantasy Football 2025 Season Stats
-from Python.extract_stats import *
+from extract_stats import web_to_table
 import pandas as pd
 
 # Objective of this file:
@@ -8,7 +8,7 @@ web_link = 'https://fantasydata.com/nfl/fantasy-football-leaders?page=1'
 headers, stats = web_to_table(web_link)
 
 # 2. Create a dataframe based on the headers and data collected
-fantasy_football_df = pd.DataFrame(stats,columns=headers)
+fantasy_football_df = pd.DataFrame(stats, columns=headers)
 
 # 3. Export a CSV file from the giant table
 fantasy_football_df.to_csv('fantasy_football_stats.csv')
